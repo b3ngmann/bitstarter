ec40bb12c6d3b26a5d4309c971a401ab4a6cb75d
Eric Engmann <b3ngmann@gmail.com>
2013-07-09 20:50:13 +0000

    modified bracket error in main function

--- web.js
+++ web.js
@@ -4,12 +4,11 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response){
-
-   fs.readFile('index.html',function (err, data)) {
-   if (err) throw err;
-   response.end(data); 
- }
-});
+   fs.readFile('index.html',function (err, data) {
+	 if (err) throw err;
+	response.end(data); 
+   }
+}));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
