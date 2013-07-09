ddc3b658657bbc948e21edc3fe537ec733502f54
Eric Engmann <b3ngmann@gmail.com>
2013-07-09 20:55:18 +0000

    correction on closure of main func

--- web.js
+++ web.js
@@ -7,8 +7,8 @@ app.get('/', function(request, response){
    fs.readFile('index.html',function (err, data) {
 	 if (err) throw err;
 	response.end(data); 
-   }
-}));
+   });
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
