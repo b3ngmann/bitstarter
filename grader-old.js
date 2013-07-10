970e1ec5f66b160dbc3c1dc8801fc8097a844e8c
Eric Engmann <b3ngmann@gmail.com>
2013-07-10 15:40:08 +0000

    url read component added to grader.js

--- grader-old.js
+++ grader-old.js
@@ -26,7 +26,6 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-var rest = require('restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -38,7 +37,7 @@ var assertFileExists = function(infile) {
 };
 
 var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(htmlfile);
+    return cheerio.load(fs.readFileSync(htmlfile));
 };
 
 var loadChecks = function(checksfile) {
@@ -62,37 +61,14 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
-//my url addition
-var readFile = function (file) {
-  return fs.readFileSync(file);
-}
-//end of url addition
-
 if(require.main == module) {
-    var outJson, checkJson;
-
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-u, --url <url>', 'Path to url')
         .parse(process.argv);
-
-    if (program.url) {
-        rest.get(program.url).on('complete', function (result) {
-
-            checkJson = checkHtmlFile(result, program.checks);
-            outJson = JSON.stringify(checkJson, null, 4);
-            console.log(outJson);
-          });
-      
-    }
-    else if (program.file) {
-      checkJson = checkHtmlFile(readFile(program.file), program.checks);
-      outJson = JSON.stringify(checkJson, null, 4);
-      console.log(outJson);
-    }
-} 
-else {
-    exports.loadChecks = loadChecks;
+    var checkJson = checkHtmlFile(program.file, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+} else {
     exports.checkHtmlFile = checkHtmlFile;
 }
